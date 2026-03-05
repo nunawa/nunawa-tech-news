68ddb164f85d8a2c38e171aa4d6c1916abc53c81
nunawa <71294849+nunawa@users.noreply.github.com>
2026-03-06 00:09:18 +0900

    Add originalTitle field

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const articleSchema = z.object({
 	title: z.string().min(1),
+	originalTitle: z.string().default(""), // 2026-03-06以前はoriginalTitleがないため、デフォルト値を空文字にする
 	link: z.string().url(),
 	publishDate: z.coerce.date(),
 	content: z.string().min(1),
